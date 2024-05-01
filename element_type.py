38365ffa0416c6606ea5856d413fdf70b56532fb
ItsNotCam <camallanyoung@gmail.com>
2024-05-01 11:39:14 -0400

    removed useless file

--- element_type.py
+++
@@ -1,7 +0,0 @@
-class Element_Type():
-	def __init__(self, pokemon_number, pokemon_type):
-		self.pokemon_number = pokemon_number
-		self.pokemon_type = pokemon_type
-
-	def to_tuple(self):
-		return (self.pokemon_number, self.pokemon_type)
