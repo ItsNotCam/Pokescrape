d9bf9d8080f49de4858d5b0ca54bbd3001824db6
ItsNotCam <camallanyoung@gmail.com>
2024-05-06 20:45:04 -0400

    removed useless files

--- objects/ability.py
+++
@@ -1,8 +0,0 @@
-class Ability:
-	def __init__(self, name, description, generation):
-		self.name = name
-		self.description = description
-		self.generation = generation
-
-	def to_tuple(self):
-		return (self.name, self.description, self.generation)
